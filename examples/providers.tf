cebd124725b2ef1e19f3ac25b8adc2a0afab3455
Vivek <vivek@gmail.com>
2022-12-14 21:19:15 +0530

    Added files

---
+++ examples/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  # see https://releases.hashicorp.com/terraform/
+  required_version = ">= 0.13.1"
+  backend "gcs" {
+    bucket = "cloudorbit"
+    prefix = "main"
+  }
+  required_providers {
+    # see https://github.com/terraform-providers/terraform-provider-google/releases
+    google = {
+      source  = "hashicorp/google"
+      version = "3.68.0" 
+    }
+  }
+}
+
+provider "google" {
+  project = var.gcp_project_id
+}
---
+++ examples/variables.tf
@@ -0,0 +1,7 @@
+## --- REQUIRED PARAMETERS ------------------------------------------------------------------------------------------------
+
+variable "gcp_project_id" {
+  type        = string
+  default     = ""
+  description = "Id of the GCP project"
+}
